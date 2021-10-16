79d52ad6ab7084107bb7d11a525f1647fc2a2162
toprakyasin0101@gmail.com <toprakyasin0101@gmail.com>
2021-10-17 01:33:08 +0300

    aa

---
+++ outKeyword/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace outKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2 = 15;
+
+            var result2 = Add(out number1, number2);
+
+            Console.WriteLine(result2);
+
+        }
+
+        static int Add(out int number1, int number2)
+        {
+            number1 = 30;
+            return number1 + number2;
+        }
+    }
+    
+}
---
+++ refKeyword/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace refKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            int number2 = 15;
+
+            var result2 = Add(ref number1, number2);
+
+            Console.WriteLine(result2);
+
+        }
+
+        static int Add(ref int number1, int number2)
+        {
+            number1 = 30;
+            return number1 + number2;
+        }
+    }
+    
+}
