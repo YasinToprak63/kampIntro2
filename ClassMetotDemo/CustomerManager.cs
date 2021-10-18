db596dce228fda7e8fade4cf966ceeec6109dbf1
toprakyasin0101@gmail.com <toprakyasin0101@gmail.com>
2021-10-19 01:37:19 +0300

    Project

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void List(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.LastName +" : " + "Customer Listed!");
+            
+        }
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.LastName + " : " + "Customer Added!");
+     
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.LastName + " : " + " Customer Deleted!");
+            
+        }
+        public void Update(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.LastName + " : " + " Customer Updated!");
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Ahmet";
+            customer1.LastName = "Toprak";
+            customer1.YearOfBirth = 1995;
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Yasin";
+            customer2.LastName = "Kaya";
+            customer2.YearOfBirth = 2000;
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.Name = "Nihat";
+            customer3.LastName = "Kahveci";
+            customer3.YearOfBirth = 1891;
+
+
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Delete(customer2);
+            customerManager.List(customer3);
+            customerManager.Update(customer1);
+
+
+
+        }
+    }
+}
